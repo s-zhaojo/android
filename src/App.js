25d99a4a12021073a4d5d8ec69035602f193bf13
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-05-30 19:28:23 -0700

    Added Picture for App.JS

--- src/App.js
+++ src/App.js
@@ -303,8 +303,8 @@ const MapComponent = () => {
       </div>
 
       {/* User Profile Card */}
-      <div className="user-profile">
-        <img src="https://via.placeholder.com/250x150" alt="John" />
+      <div className="user-profile">Add commentMore actions
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fjoshzhao7&psig=AOvVaw2CTZy0nbOoITCKrhmeyR_H&ust=1746328999383000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCJiumNGsho0DFQAAAAAdAAAAABAE" alt="John" />
         <h1>John Doe</h1>
         <p className="title">CEO & Founder, Example</p>
         <p>Harvard University</p>
@@ -319,5 +319,6 @@ const MapComponent = () => {
     </div>
   );
 };
+
 
 export default MapComponent;
