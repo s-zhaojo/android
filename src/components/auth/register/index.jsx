e1bff123f25ff9236bad82913f3279dacdc3d6dc
s-zhaojo <56804773+s-zhaojo@users.noreply.github.com>
2025-06-01 17:04:49 -0700

    import. index.css

--- src/components/auth/register/index.jsx
+++ src/components/auth/register/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Navigate, Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../../contexts/authContext'
 import { doCreateUserWithEmailAndPassword } from '../../../firebase/auth'
-
+import './index.css';
 const Register = () => {
 
     const navigate = useNavigate()
